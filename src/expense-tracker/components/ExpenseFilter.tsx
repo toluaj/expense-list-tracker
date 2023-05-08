bcb20fd1876f274695c7bca0923c27c39958bd1d
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-08 20:05:39 +0100

    feat: filter through expense list by category

---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,18 @@
+import { categories } from "../categories";
+
+interface ExpenseFilterProps {
+    onSelectCategory: (category: string) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: ExpenseFilterProps) => {
+  return (
+    <select className='form-select' onChange={(event) => onSelectCategory(event.target.value)}>
+        <option value="">All categories</option>
+        {categories.map(category => (
+            <option key={category} value={category}>{category}</option>
+        ))}
+    </select>
+  )
+}
+
+export default ExpenseFilter
