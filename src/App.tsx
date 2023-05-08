56a2943e3b8f1e171ce59610a06c316f36eb5174
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-08 20:08:10 +0100

    chore: add app components to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { useState } from "react"
-import Alert from "./components/Alert"
-import Button from "./components/Button"
-import DismissableAlert from "./components/DismissableAlert"
-import {ListGroup} from "./components/ListGroup"
-import Message from "./Message"
 import './App.css'
-import Like from "./components/Like"
+import './index.css'
+import ExpenseList from "./expense-tracker/components/ExpenseList"
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter"
+import ExpenseForm from "./expense-tracker/components/ExpenseForm"
 
 function App() {
-    const [showAlert, setshowAlert] = useState(false)
-   
-    const onClick = () => {
-      setshowAlert(!showAlert)
+    const [selectedCategory, setSelectedCategory] = useState('')
+    const [expenses, setExpenses] = useState([
+      { id: 0, description: '', amount: 0, category: '' },
+])
+
+    const onDelete = (id: number) => {
+      setExpenses(expenses.filter(x => x.id!== id))
     }
+
+    const filteredExpenses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses.splice(1)
+    
     return (
     <div>
-    {/* {showAlert && <DismissableAlert onClick={onClick} />} */}
-    {/* <Button onClick={onClick}>My button</Button> */}
-    {/* <ListGroup /> */}
-    {/* <Button children="Canvas View" onClick={() => alert("Clicked this!")} /> */}
-    <Like onClick={() => console.log('clicked')} />
+      <div className="mb-5">
+      <ExpenseForm onSubmit={(expense) => setExpenses([...expenses, { id: expenses.length + 1, ...expense }])} />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)} />
+      </div>
+      <ExpenseList expenses={filteredExpenses} onDelete={onDelete} />
     </div>  
     )
 }
